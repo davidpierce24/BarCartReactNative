012ce604e1d2a990125cb9f7d7372043e3d1e079
davidpierce24 <pie2ce@gmail.com>
2023-04-07 20:15:50 -0400

    search updates

--- components/BottomNavigation.tsx
+++ components/BottomNavigation.tsx
@@ -1,14 +1,27 @@
 import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
-import Feather from 'react-native-vector-icons/Feather';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import AddButton from './AddButton';
 
 const BottomNavigation = () => {
 
 return (
     <View className='bg-cyan-800'>
-        <View className='flex items-center pt-4 pb-2 '>
-
+        <View className='flex flex-row justify-between pt-4 pb-4 px-8'>
+            <TouchableOpacity>
+                <FontAwesome5 name="home" size={30} color="#ffffff"/>
+            </TouchableOpacity>
+            <TouchableOpacity className='mr-16'>
+                <FontAwesome name="search" size={30} color="#ffffff"/>
+            </TouchableOpacity>
+            <TouchableOpacity className=''>
+                <FontAwesome5 name="list-ul" size={30} color="#ffffff"/>
+            </TouchableOpacity>
+            <TouchableOpacity>
+                <FontAwesome5 name="glass-whiskey" size={30} color="#ffffff"/>
+            </TouchableOpacity>
+            
         </View>
     </View>
     )
